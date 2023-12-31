9ba86e990bd3eca95a58d3c1e274e348bdc55b1b
Karpov Sergey <karpovsergey1990@gmail.com>
2023-12-31 11:46:52 +0400

    add JWT, get user profile

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,7 @@
 package main
 
-import (
-	"GoSocial/internal/handlers"
-	"github.com/labstack/echo/v4"
-)
+import "GoSocial/internal/app"
 
 func main() {
-	e := echo.New()
-	e.POST("/login", handlers.Login)
-	e.POST("/registration", handlers.Registration)
-	e.Logger.Fatal(e.Start(":3336"))
+	app.Start()
 }
